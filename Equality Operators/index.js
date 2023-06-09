3954d14789610e12ff53bbc37de813cc48584534
98Ganesham <98ganesham@gmail.com>
2023-06-09 15:44:36 +0630

    JAVa-Script

---
+++ Equality Operators/index.js
@@ -0,0 +1,18 @@
+// Strict Equality Operator
+console.log(1===1);
+// it will get true
+
+// it will get false;
+console.log('1'===1);
+
+
+// Lose Equality Operators
+console.log(1==1);
+// it will get True;
+// it will also get true;
+console.log('1'==1);
+console.log(true==1);
+// it will get false;
+console.log(false==1);
+console.log(false==true);
+console.log(true==0);
